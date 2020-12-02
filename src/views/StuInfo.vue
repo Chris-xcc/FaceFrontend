2d38da454e0a98186a3b14e43ed956ee8d1ad93b
Chris-xcc <chris_guoc@163.com>
2020-12-02 19:32:18 +0800

    stu info

---
+++ src/views/StuInfo.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-table
+    stripe
+    :data="stuData"
+    style="width: 100%"
+    :header-cell-style="headClass"
+    align="center"
+    :cell-style="rowClass"
+  >
+    <el-table-column prop="id" label="序号"> </el-table-column>
+    <el-table-column prop="number" label="学号"> </el-table-column>
+    <el-table-column prop="username" label="姓名"> </el-table-column>
+    <el-table-column label="性别">
+      <template slot-scope="scope">
+        {{ scope.row.sex === "male" ? "男" : "女" }}
+      </template>
+    </el-table-column>
+    <el-table-column label="人脸图片">
+      <template slot-scope="scope">
+        <!-- <img :src="require('http:localhost:8000/media/'+scope.row.face)" alt="" style="width:60px" /> -->
+        <!-- <img src="http://localhost:8000/media/face_img/2017127249/2020120112062773.jpg" alt="" style="width:60px" /> -->
+        <img
+          :src="'http://localhost:8000/media/' + scope.row.face"
+          alt=""
+          style="width: 60px"
+        />
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { get } from "../utils/request";
+import { getCookie } from "../utils/cookie";
+
+export default {
+  name: "StuInFo",
+  data() {
+    return {
+      stuData: [],
+      stu_face: [],
+    };
+  },
+  methods: {
+    headClass() {
+      //表头居中显示
+      return "text-align:center";
+    },
+    rowClass() {
+      //表格数据居中显示
+      return "text-align:center";
+    },
+    getStuInfo() {
+      get({
+        url: "/user/",
+        headers: { "X-CSRFToken": getCookie("csrftoken") },
+      }).then((res) => {
+        console.log(res.data);
+
+        this.stuData = res.data;
+      });
+    },
+  },
+  mounted() {
+    this.getStuInfo();
+  },
+};
+</script>
+<style lang="less" scoped>
+</style>

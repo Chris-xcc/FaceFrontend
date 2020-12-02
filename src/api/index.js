0257f72c89db9d76815b3d75479df0868a0dc02e
Chris-xcc <chris_guoc@163.com>
2020-12-02 19:38:44 +0800

    api

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import service from "../utils/request";
+import store from "../store";
+
+// // 用户
+// export const UserRegister = () => {
+//     return service("/user/")
+// }
+// // 文章总数
+// export const TotalPost = () => {
+//     return get("/posts/")
+// }
+// // 分类列表
+// export const CategoryList = () => {
+//     return get("/categorys/")
+// }
+// // 标签列表
+// export const TagList = () => {
+//     return get("/tags/")
+// }
+

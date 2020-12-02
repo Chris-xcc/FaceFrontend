94eb90bd35b4b081d827d889a50482e1e79aa5cb
Chris-xcc <chris_guoc@163.com>
2020-12-02 19:31:59 +0800

    sign

---
+++ src/views/Sign.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="sign">
+    <el-table
+      :data="signData"
+      border
+      style="width: 100%"
+      stripe
+      :header-cell-style="headClass"
+      align="center"
+      :cell-style="rowClass"
+      @row-click="openDetails"
+    >
+      <el-table-column label="标题">
+        <template slot-scope="scope">
+          <span>
+            {{ scope.row.title }}
+          </span>
+        </template>
+      </el-table-column>
+      <el-table-column label="描述">
+        <template slot-scope="scope">
+          <span>
+            {{ scope.row.description }}
+          </span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { get } from "../utils/request";
+import { getCookie } from "../utils/cookie";
+import { GET_SIGNID } from "../store/mutations-types";
+
+export default {
+  name: "Sign",
+  data() {
+    return {
+      signData: [],
+    };
+  },
+  methods: {
+    headClass() {
+      //表头居中显示
+      return "text-align:center";
+    },
+    rowClass() {
+      //表格数据居中显示
+      return "text-align:center";
+    },
+    getSign() {
+      get({
+        url: "/sign/",
+        headers: { "X-CSRFToken": getCookie("csrftoken") },
+      }).then((res) => {
+        console.log(res.data);
+
+        this.signData = res.data;
+      });
+    },
+    openDetails(row) {
+      window.sessionStorage.setItem("signId", row.id);
+      this.$store.commit({
+        type: GET_SIGNID,
+        signId: row.id,
+      });
+      this.$router.push({
+        name: "Face",
+      });
+    },
+  },
+  mounted() {
+    this.getSign();
+  },
+};
+</script>
+
+<style scoped>
+span {
+  cursor: pointer;
+}
+</style>

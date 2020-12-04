0fd6d19cc534f9619427722ee34d8f57149ac273
Chris-xcc <chris_guoc@163.com>
2020-12-04 11:21:35 +0800

    fix superuser auth

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="right">
         <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-
+          default-active="1"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
           :router="true"
         >
           <el-menu-item index="/">
-            <i class="el-icon-menu"></i>
+            <i class="el-icon-s-home"></i>
             <span slot="title">首页</span>
           </el-menu-item>
 
           <el-menu-item index="/user">
-            <i class="el-icon-setting"></i>
+            <i class="el-icon-s-custom"></i>
             <span slot="title">个人中心</span>
           </el-menu-item>
           <el-menu-item index="/sign">
-            <i class="el-icon-setting"></i>
+            <i class="el-icon-s-grid"></i>
             <span slot="title">签到管理</span>
           </el-menu-item>
-          <el-menu-item index="/stu" v-if="this.$store.state.is_superuser === true">
-            <i class="el-icon-setting"></i>
+          <el-menu-item index="/stu" v-if="isSuperUser">
+            <i class="el-icon-s-order"></i>
             <span slot="title">学生信息</span>
           </el-menu-item>
         </el-menu>
@@ -34,6 +32,12 @@
 <script>
 export default {
   name: "Right",
+  methods:{
+    isSuperUser(){
+      const is_superuser = window.sessionStorage.getItem('is_superuser')
+      return is_superuser === 'true';
+    }
+  }
 };
 </script>
 <style lang="less" scoped>
@@ -42,5 +46,6 @@ export default {
 }
 .el-menu {
   height: 100%;
+  border-right: 0;
 }
 </style>

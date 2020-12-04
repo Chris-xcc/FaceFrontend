750883ae1a0d488e7fd7d4687dbbaf4464f4a6bb
Chris-xcc <chris_guoc@163.com>
2020-12-04 11:20:53 +0800

    add background image

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>首页</div>
+  <div class="index">
+    <img src="../assets/img/Background.jpg" alt="" />
+  </div>
 </template>
+<script>
+export default {
+  name: "Index",
+};
+</script>
+
+<style lang="less" scoped>
+.index {
+  background-color: RGB(3, 53, 148);
+  img {
+    width: 100%;
+  }
+}
+</style>

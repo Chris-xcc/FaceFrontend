27319eb775219c14ed084c1e1f7eb3f5cea0f2aa
Chris-xcc <chris_guoc@163.com>
2020-12-06 18:02:28 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home'
 import store from '../store'
 
 Vue.use(VueRouter)
@@ -8,12 +7,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home'),
+    component: () => import('../views/Home/Home'),
     children: [
       {
         path: '/',
         name: 'Index',
-        component: () => import('../components/Index'),
+        component: () => import('../views/Home/Index'),
       },
       {
         path: '/sign',
@@ -27,19 +26,28 @@ const routes = [
       }, {
         path: '/face',
         name: 'Face',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('../views/Face')
       },
       {
         path: '/stu',
         name: 'StuINfo',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('../views/StuInfo')
       },
+      {
+        path: '/sign_create',
+        name: 'SignCreate',
+        component: () => import('../views/SignCreate')
+      },
+      {
+        path: '/sign_info',
+        name: 'SignInfo',
+        component: () => import('../views/SignInfo'),
+      },
+      {
+        path: '/sign_info/:id',
+        name: 'SignInfoDetail',
+        component: () => import('../views/SignInfoDetail'),
+      }
 
     ]
   },
@@ -53,22 +61,7 @@ const routes = [
     name: 'Register',
     component: () => import('../views/Register')
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import('../views/About')
-  // }
-  // ,{
-  //   path: '/a',
-  //   name: 'A',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import('../views/A')
-  // }
+
 
 ]
 
@@ -90,4 +83,24 @@ router.beforeEach(function (to, from, next) {
   }
   next()
 })
+
+router.beforeEach(function (to, from, next) {
+
+  if (store.state.is_superuser === true || store.state.is_superuser === 'true') {
+    next()
+  }
+  else {
+    if (to.path === '/sign_info' || to.path === '/stu' || to.path === '/sign_create') {
+      return next('/')
+    }
+  }
+  next()
+
+})
+
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 export default router

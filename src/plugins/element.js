3577aa5376f3382e3cbd4ce20cde028f3b607cf5
Chris-xcc <chris_guoc@163.com>
2020-12-06 22:37:28 +0800

    update

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,41 @@
 import Vue from 'vue';
 import {
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  MenuItem,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Form, 
+  FormItem, 
+  Input, 
+  Button, 
+  Table,
+  TableColumn,
   Row,
-  Form, FormItem,Input,Button,Col
+  Col,
+  Upload,
 } from 'element-ui';
 
-Vue.use(Row);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
 Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
 Vue.use(Button);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Row);
 Vue.use(Col);
+Vue.use(Upload);
 
